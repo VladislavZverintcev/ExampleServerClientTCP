6d3f39e175c94b7dd2e46598cceb997ad4f8af10
VladislavZverincev <hopmxod007@mail.ru>
2021-10-17 14:43:07 +0700

    Добавьте файлы проекта.

---
+++ ExampleClientTCP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ExampleClientTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+            
+            while(true)
+            {
+                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Enter your message");
+                var message = Console.ReadLine();
+                var data = Encoding.UTF8.GetBytes(message);
+                tcpSocket.Connect(tcpEndPoint);
+                tcpSocket.Send(data);
+                var buffer = new byte[256];
+                var size = 0;
+                var answer = new StringBuilder();
+                do
+                {
+                    size = tcpSocket.Receive(buffer);
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (tcpSocket.Available > 0);
+                Console.WriteLine(answer.ToString());
+                tcpSocket.Shutdown(SocketShutdown.Both);
+                tcpSocket.Close();
+            }
+            
+        }
+    }
+}
